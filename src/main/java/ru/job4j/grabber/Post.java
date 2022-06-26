bc448624774ea44c8b5b4a9d1f3b45127f800a2f
Sonder82 <Himrs82@gmail.com>
2022-06-26 23:59:57 +0500

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,47 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Модель Post
+ * id идентификатор вакансии (берется из нашей базы данных)
+ * link ссылка на описание вакансии
+ * description описание вакансии
+ * created дата создания вакансии
+ */
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
